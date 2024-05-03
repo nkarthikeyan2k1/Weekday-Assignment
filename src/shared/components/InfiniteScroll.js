39e71d55eed318faf57f7a510bf6e615bd884b69
nkarthikeyan2k1 <nkarthikeyan2k1@gmail.com>
2024-05-03 21:14:40 +0530

    code cleanup

--- src/shared/components/InfiniteScroll.js
+++ src/shared/components/InfiniteScroll.js
@@ -3,29 +3,17 @@ import { throttleFunction } from "../../services/UtlitsServices";
 
 const InfiniteScroll=({loadMore})=>{
 
-  const [scroll,setScroll]=useState({
-    inner:window.innerHeight,
-    top:document.documentElement.scrollTop,
-    off:document.documentElement.offsetHeight
-  })
-
     useEffect(()=>{
       document.addEventListener('scroll',throttleHandelScroll,{passive:true})
         return ()=>document.removeEventListener('scroll',throttleHandelScroll)
     },[loadMore])
 
     const handelScroll=()=>{
-      setScroll({
-        inner:window.innerHeight,
-      top:document.documentElement.scrollTop,
-      off:document.documentElement.offsetHeight
-      })
 
       const scrollTop = document.documentElement.scrollTop;
       const innerHeight = window.innerHeight;
       const offsetHeight = document.documentElement.offsetHeight;
 
-          // if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight) {
             if (scrollTop >= offsetHeight - innerHeight - 1 && scrollTop <= offsetHeight - innerHeight + 1) {
             loadMore();
           }
@@ -35,11 +23,6 @@ const InfiniteScroll=({loadMore})=>{
 
     // handel the scroll evevnt repeted check using the throllefunction
     const throttleHandelScroll=throttleFunction(handelScroll,500)
-
-    return <div style={{position:"fixed",zIndex:"999",background:"aliceblue"}} >
-      scroll {scroll.inner} { scroll.top} {scroll.off} <br />
-      checking {scroll.inner+scroll.top}'===' {scroll.off}
-      </div>
 }
 
 export default React.memo(InfiniteScroll);
